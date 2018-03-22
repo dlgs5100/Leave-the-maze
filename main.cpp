770060b312a7817d9e4c82a350fdfe4b239267b3
dlgs5100 <dlgs5100@gmail.com>
2018-03-22 21:01:26 +0800

    Help Function complete

---
+++ main.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+#include<fstream>
+#include <unistd.h>
+using namespace std;
+
+struct Room {
+	short status[5];
+};
+struct Point {
+	int x;
+	int y;
+};
+void printHelp();
+bool playing(Room **room, Point *Entrance, Point *Exit, int direction);
+int main(int argc, char* argv[]) {
+
+	int x, y;
+	Room **room = NULL;
+	Point *Entrance = new Point, *Exit = new Point;
+
+	char c;
+	while((c=getopt(argc, argv, "fh:")) != -1){
+		switch(c)
+    		{
+        	case'f':
+		printHelp();
+		break;
+    		}
+	}
+
+	ifstream inputFile("mazefile.txt");
+	inputFile >> x >> y;
+
+	room = new Room*[x];
+	for (int i = 0; i < y; i++)
+	{
+		room[i] = new Room[y];
+	}
+
+
+	for (int i = 0; i < x*y; i++)
+	{
+		for (int j = 0; j < 5; j++) {
+			inputFile >> room[i / x][i%x].status[j];
+			if (room[i / x][i%x].status[j] == 4)
+			{
+				Entrance->x = i / x;
+				Entrance->y = i % x;
+			}
+			else if (room[i / x][i%x].status[j] == 3)
+			{
+				Exit->x = i / x;
+				Exit->y = i % x;
+			}
+		}
+	}
+
+	int direction;
+	while (1) {
+		cin >> direction;
+		if (playing(room, Entrance, Exit, direction)) {
+			cout << "Finish" << endl;
+			break;
+		}
+	}
+
+
+	return 0;
+}
+void printHelp() {
+	cout << "<<Leave the maze>>" << endl;
+	cout <<	"Rule:" << endl;
+	cout << "1 moves to upper." << endl;
+	cout << "2 moves to left." << endl;
+	cout << "3 moves to right." << endl;
+	cout << "4 moves to below." << endl;
+}
+bool playing(Room **room, Point *Entrance, Point *Exit, int direction) {
+	switch (direction)
+	{
+	case 1:
+		if (room[Entrance->x][Entrance->y].status[0] == 2)
+			Entrance->x -= 1;
+		else
+			cout << "Wall!" << endl;
+		break;
+	case 2:
+		if (room[Entrance->x][Entrance->y].status[1] == 2)
+			Entrance->y -= 1;
+		else
+			cout << "Wall!" << endl;
+		break;
+	case 3:
+		if (room[Entrance->x][Entrance->y].status[2] == 2)
+			Entrance->y += 1;
+		else
+			cout << "Wall!" << endl;
+		break;
+	case 4:
+		if (room[Entrance->x][Entrance->y].status[3] == 2)
+			Entrance->x += 1;
+		else
+			cout << "Wall!" << endl;
+		break;
+	default:
+		break;
+	}
+	if (Entrance->x == Exit->x && Entrance->y == Exit->y)
+		return true;
+	else
+		return false;
+}
