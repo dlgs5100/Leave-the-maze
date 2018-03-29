509937167e361243486b66b3a31047c8af7b62b0
dlgs5100 <dlgs5100@gmail.com>
2018-03-29 23:13:52 +0800

    Add pthread and incomplete initial maze function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
 #include<fstream>
-#include <unistd.h>
+#include<unistd.h>
+#include<pthread.h>
 using namespace std;
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t condition_var = PTHREAD_COND_INITIALIZER;
+
+int direction;
+bool isClear = false;
 extern char *optarg;
 struct Room {
 	short status[5];
@@ -11,48 +17,73 @@ struct Point {
 	int x;
 	int y;
 };
+
+Room **room = NULL;
+Point *Entrance = new Point, *Exit = new Point;
+void *command_Thread_func(void* i);
+void *main_Thread_func(void* i);
 void printHelp();
 void initMaze(Room **&room, Point *Entrance, Point *Exit);
 bool playing(Room **room, Point *Entrance, Point *Exit, int direction);
-int main(int argc, char* argv[]) {
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg);
 
-	
-	Room **room = NULL;
-	Point *Entrance = new Point, *Exit = new Point;
+int main(int argc, char* argv[]) {
 
 	char c;
 	while((c=getopt(argc, argv, "hf:")) != -1){
 		switch(c)
     		{
 		case'f':
-		initMaze(room, Entrance, Exit);
-		int direction;
-		while (1) {
 
-			cin >> direction;
-
-			if (playing(room, Entrance, Exit, direction)) {
-
-				cout << "Finish" << endl;
-
-				break;
-
-			}
-
-		}
+		pthread_t command_Thread, main_Thread;
+	
+		pthread_create(&command_Thread, NULL, &command_Thread_func, NULL);
+		pthread_create(&main_Thread, NULL, &main_Thread_func, NULL);
+		
+		pthread_join(command_Thread, NULL);
+		pthread_join(main_Thread, NULL);
+		
 		break;
 
-        	case'h':
+        case'h':
 		printHelp();
+		
 		break;
 
 		default:
 		cout << "Error command" << endl;
+		
 		break;
     		}
 	}
 
 	return 0;
+}
+void *command_Thread_func(void* i){
+	
+	initMaze(room, Entrance, Exit);
+	while (1) {
+		pthread_mutex_lock(&mutex);
+		
+		cin >> direction;
+		
+		pthread_cond_wait(&condition_var, &mutex);
+		
+		if(isClear){
+			cout << "Finish" << endl;
+			return NULL;
+		}
+		
+		pthread_mutex_unlock(&mutex);
+	}
+}
+void *main_Thread_func(void* i){
+	pthread_mutex_lock(&mutex);
+	
+	isClear = playing(room, Entrance, Exit, direction);
+	pthread_cond_signal(&condition_var);
+		
+	pthread_mutex_unlock(&mutex);
 }
 void printHelp() {
 	cout << "<<Leave the maze>>" << endl;
@@ -95,26 +126,34 @@ bool playing(Room **room, Point *Entrance, Point *Exit, int direction) {
 	switch (direction)
 	{
 	case 1:
-		if (room[Entrance->x][Entrance->y].status[0] == 2)
+		if (room[Entrance->x][Entrance->y].status[0] == 2){
 			Entrance->x -= 1;
+			cout << "Turn up!" << endl;
+		}
 		else
 			cout << "Wall!" << endl;
 		break;
 	case 2:
-		if (room[Entrance->x][Entrance->y].status[1] == 2)
+		if (room[Entrance->x][Entrance->y].status[1] == 2){
 			Entrance->y -= 1;
+			cout << "Turn left!" << endl;
+		}
 		else
 			cout << "Wall!" << endl;
 		break;
 	case 3:
-		if (room[Entrance->x][Entrance->y].status[2] == 2)
+		if (room[Entrance->x][Entrance->y].status[2] == 2){
 			Entrance->y += 1;
+			cout << "Turn right!" << endl;
+		}
 		else
 			cout << "Wall!" << endl;
 		break;
 	case 4:
-		if (room[Entrance->x][Entrance->y].status[3] == 2)
+		if (room[Entrance->x][Entrance->y].status[3] == 2){
 			Entrance->x += 1;
+			cout << "Turn down!" << endl;
+		}
 		else
 			cout << "Wall!" << endl;
 		break;
